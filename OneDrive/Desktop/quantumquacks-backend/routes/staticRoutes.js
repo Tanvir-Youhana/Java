b0245f0f3542fb9f515568c4c8f698807994d730
Tanvir-Youhana <44341296+Tanvir-Youhana@users.noreply.github.com>
2021-12-16 21:33:11 -0500

    Delete OneDrive/Desktop/quantumquacks-backend directory

--- OneDrive/Desktop/quantumquacks-backend/routes/staticRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"; 
-import {
-    getTrendingTickers
-} from "../controllers/Static.js";
-
-const router = express.Router(); 
-
-router.get('/test', getTrendingTickers);
